0cdc19149f026e1774562b57e7be9c4cd92009db
Jnadaldevwwm <julien.devwwm79@gmail.com>
2020-12-21 10:12:38 +0100

    responsiv cards

---
+++ Controlers/controlerUsers.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once '../views/view.php';
+//require_once '../Models/users.php';
+
+class ControlerUsers{
+
+    public function __construct(){
+
+    }
+
+    public function signIn(){
+
+        $view = new View('signIn');
+        $view->render(array());
+    }
+}
