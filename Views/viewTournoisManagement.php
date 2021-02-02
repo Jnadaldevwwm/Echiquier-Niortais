37ae1ac2971ad9cc45dfdb0a88feff1fdc2a8182
Jnadaldevwwm <julien.devwwm79@gmail.com>
2021-02-02 12:04:39 +0100

    responsiv calendrier

--- Views/viewTournoisManagement.php
+++ Views/viewTournoisManagement.php
@@ -1,3 +1,9 @@
 <?php
     $this->title = "Gestion des tournois";
 ?>
+<span><a href="?action=indexAdmin"><-Retour Index</a></span>
+<h1>Gestion des tournois</h1>
+<br>
+<h2>Que souhaitez-vous faire ?</h2>
+<div>Consulter les tournois</div>
+<div>Créer un nouveau tournoi</div>
